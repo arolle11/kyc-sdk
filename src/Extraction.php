bf922d0d8062998736d9768d84dfa31b842216aa
Arolle11 <arollefona11@gmail.com>
2024-12-28 23:42:03 +0100

    modify dependencies

--- src/Extraction.php
+++ src/Extraction.php
@@ -6,6 +6,22 @@ namespace KYCSDK;
 class extractor {
 
     public function extract_data(){
+    
+    }
 
+    private function processText(string $text): array
+    {
+        // Example simplistic text processing
+        $lines = explode("\n", $text);
+        $data = [];
+        foreach ($lines as $line) {
+            if (strpos($line, 'Name:') !== false) {
+                $data['name'] = trim(str_replace('Name:', '', $line));
+            }
+            if (strpos($line, 'ID Number:') !== false) {
+                $data['id_number'] = trim(str_replace('ID Number:', '', $line));
+            }
+        }
+        return $data;
     }
 }
