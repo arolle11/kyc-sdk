20ae9bd6e41ec0df3f96c73e3f807f8c853b0dfd
Arolle11 <arollefona11@gmail.com>
2024-12-29 21:40:23 +0100

    modify dependencies

--- src/Extraction.php
+++ src/Extraction.php
@@ -9,19 +9,5 @@ class extractor {
     
     }
 
-    private function processText(string $text): array
-    {
-        // Example simplistic text processing
-        $lines = explode("\n", $text);
-        $data = [];
-        foreach ($lines as $line) {
-            if (strpos($line, 'Name:') !== false) {
-                $data['name'] = trim(str_replace('Name:', '', $line));
-            }
-            if (strpos($line, 'ID Number:') !== false) {
-                $data['id_number'] = trim(str_replace('ID Number:', '', $line));
-            }
-        }
-        return $data;
-    }
+
 }
